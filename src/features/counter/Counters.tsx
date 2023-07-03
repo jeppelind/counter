33acfb65613353a89eaac7dfc279578ab706d147
jeppelind <58559776+jeppelind@users.noreply.github.com>
2023-07-03 12:34:57 +0200

    Update CSS to increase readability

--- src/features/counter/Counters.tsx
+++ src/features/counter/Counters.tsx
@@ -34,7 +34,7 @@ const Counters = () => {
           <Card.Body>
             <Card.Title>No counters found!</Card.Title>
             <Card.Text>
-              There does not seem to be any counters saved
+              Create a new one to get started.
             </Card.Text>
           </Card.Body>
         </Card>
